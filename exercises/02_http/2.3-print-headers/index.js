9f21e8e73f0c1f9f3a74faac1c419c79143243fd
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-13 01:29:59 +0300

    Setheader and getheader added

--- exercises/02_http/2.3-print-headers/index.js
+++ exercises/02_http/2.3-print-headers/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer(function(req, res){
-    // res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
+    res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
     res.writeHead(200, {'Content-Type' : 'text/html'});
     // const header1 = res.getHeaders();
     // console.log("header 1 = ",header1);
