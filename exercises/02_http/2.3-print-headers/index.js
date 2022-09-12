b166bacd7b16646448d0841b14afc8bb0ddcfcc2
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-13 01:07:37 +0300

    Setheader and getheader added

---
+++ exercises/02_http/2.3-print-headers/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const { json } = require("stream/consumers");
+
+const server = http.createServer(function(req, res){
+    res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+
+    // const header1 = res.getHeaders();
+    // console.log("header 1 = ",header1);
+    res.write(JSON.stringify(res.getHeaders()))
+    res.end("Hello Maruf");
+});
+
+server.listen(3000, '127.0.0.1');
+console.log("Server started, listening to port 3000");
