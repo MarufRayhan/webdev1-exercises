9d7e9711628eb9ff8c6d2db191a15c7f93fd7bbc
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-13 20:03:40 +0300

    Setheader and getheader added

--- exercises/02_http/2.3-print-headers/index.js
+++ exercises/02_http/2.3-print-headers/index.js
@@ -1,43 +1,43 @@
-// const http = require("http");
-// const fs = require('fs');
-// const path = require('path');
+const http = require("http");
+const fs = require('fs');
+const path = require('path');
 
-// const server = http.createServer(function(req, res){
-//     res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
-//     res.writeHead(200, {'Content-Type' : 'text/html'});
+const server = http.createServer(function(req, res){
+    // res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
+    res.writeHead(200, {'Content-Type' : 'text/html'});
 
-//     const hasContentType = res.hasHeader('content-type');
-//     console.log("header", hasContentType)
+    const hasContentType = res.hasHeader('content-type');
+    console.log("header", hasContentType)
 
-//     res.write(JSON.stringify(res.getHeaders()))
-//     res.end();
-// }).listen(3000);
+    res.write(JSON.stringify(req.headers))
+    res.end();
+}).listen(3000);
 
 // console.log("Server started, listening to port 3000");
 
-const http = require("http");
+// const http = require("http");
 
-http.createServer((request, response)=>{
-    const { headers, method, url } = request;
-    let body = [];
-    request.on('error', (err)=>{
-        console.log(err)
-    }).on('data', (chunk)=> {
-        body.push(chunk);
-    }).on('end', ()=>{
-        body = Buffer.concat(body).toString();
+// http.createServer((request, response)=>{
+//     const { headers, method, url } = request;
+//     let body = [];
+//     request.on('error', (err)=>{
+//         console.log(err)
+//     }).on('data', (chunk)=> {
+//         body.push(chunk);
+//     }).on('end', ()=>{
+//         body = Buffer.concat(body).toString();
 
-        response.on('error', (err)=>{
-            console.log(err)
-        });
+//         response.on('error', (err)=>{
+//             console.log(err)
+//         });
 
-        response.setHeader('Content-Type', 'text/html');
+//         response.setHeader('Content-Type', 'text/html');
    
-        response.writeHead(200, {'Content-Type': 'text/html'})
-        const responseBody = { headers, method, url, body };
+//         response.writeHead(200, {'Content-Type': 'text/html'})
+//         const responseBody = { headers, method, url, body };
 
-        response.write(JSON.stringify(responseBody));
-        response.end();
-    })
+//         response.write(JSON.stringify(responseBody));
+//         response.end();
+//     })
 
-}).listen(3000);
+// }).listen(3000);
