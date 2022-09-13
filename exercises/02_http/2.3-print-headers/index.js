115e1fdb58e5109f6b8384798c6113a08f12d995
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-13 15:23:17 +0300

    Setheader and getheader added

--- exercises/02_http/2.3-print-headers/index.js
+++ exercises/02_http/2.3-print-headers/index.js
@@ -1,12 +1,43 @@
+// const http = require("http");
+// const fs = require('fs');
+// const path = require('path');
+
+// const server = http.createServer(function(req, res){
+//     res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
+//     res.writeHead(200, {'Content-Type' : 'text/html'});
+
+//     const hasContentType = res.hasHeader('content-type');
+//     console.log("header", hasContentType)
+
+//     res.write(JSON.stringify(res.getHeaders()))
+//     res.end();
+// }).listen(3000);
+
+// console.log("Server started, listening to port 3000");
+
 const http = require("http");
 
-const server = http.createServer(function(req, res){
-    res.setHeader('Set-Cookie', ['foo=bar', 'bar=baz']);
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    // const header1 = res.getHeaders();
-    // console.log("header 1 = ",header1);
-    res.write(JSON.stringify(res.getHeaders()))
-    res.end();
-}).listen(3000);
+http.createServer((request, response)=>{
+    const { headers, method, url } = request;
+    let body = [];
+    request.on('error', (err)=>{
+        console.log(err)
+    }).on('data', (chunk)=> {
+        body.push(chunk);
+    }).on('end', ()=>{
+        body = Buffer.concat(body).toString();
 
-console.log("Server started, listening to port 3000");
+        response.on('error', (err)=>{
+            console.log(err)
+        });
+
+        response.setHeader('Content-Type', 'application/json');
+   
+        response.writeHead(200, {'Content-Type': 'application/json'})
+        const responseBody = { headers, method, url, body };
+
+        response.write(JSON.stringify(responseBody));
+        response.end();
+    })
+
+}).listen(3000);
