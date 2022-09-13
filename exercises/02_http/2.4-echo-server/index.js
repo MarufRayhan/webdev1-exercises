af9ee4bce38bd4c51ea7f92a691d42ecac517032
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-14 01:30:49 +0300

    echo added

--- exercises/02_http/2.4-echo-server/index.js
+++ exercises/02_http/2.4-echo-server/index.js
@@ -1,14 +1,16 @@
 const http = require("http");
 
-const server = http.createServer(function(req, res){
+http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    let body = []
+    let body = [];
+
     req.on('data', (chunk) => {
         body.push(chunk);
       }).on('end', () => {
         body = Buffer.concat(body).toString();
-        res.end(body);
-      });
-      
+        res.end(body)
+      })
+
+      res.end();
 
 }).listen(3000);
