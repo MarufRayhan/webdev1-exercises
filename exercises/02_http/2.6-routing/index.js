e0f7c7dc91aac787a2a14e3dc6cee8d92abdbb97
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-14 16:00:17 +0300

    router added

---
+++ exercises/02_http/2.6-routing/index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer(function(request, response) {
+    if (request.url === '/respond'){
+    response.writeHead(200, { 'Content-Type':'text/html'});
+    readFileSendResponse('index.html', 'text/html', response);
+  }
+  else if (request.url === '/dystopy'){
+    response.writeHead(200, { 'Content-Type':'text/html'});
+    readFileSendResponse('bradbury.html', 'text/html', response);
+  }
+  else if (request.url === '/classical'){
+    response.writeHead(200, { 'Content-Type':'text/html'});
+    readFileSendResponse('homer.html', 'text/html', response);
+  }
+  else {
+    response.statusCode = 404;
+    response.statusMessage = 'Requested content not found';
+    response.end();
+  }
+}).listen(3000);
+
+const readFileSendResponse = (fileName, contentType, response) => {
+  fs.readFile(path.resolve(fileName), function(error, file) {
+    if (error) {
+      response.writeHead(404);
+      response.write('An error occured: ', error);
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.write(file);
+    }
+    response.end();
+  })
+}
