86764bd8969aef4680c9be91e4b0d9d6a4b9d6d3
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-14 15:31:05 +0300

    file format added

--- exercises/02_http/2.5-content-types/index.js
+++ exercises/02_http/2.5-content-types/index.js
@@ -7,7 +7,17 @@ http.createServer(function(request, response) {
   //TODO: implement sending responds to requests for XML, TXT, and */* below, handling JSON provided as an example.
   if (request.headers['accept'].includes('application/json')) {
     readFileSendResponse('data.json', 'application/json', response);
-  } else {
+  } 
+  else if(request.headers['accept'].includes('application/xml')){
+    readFileSendResponse('data.xml', 'application/xml', response);
+  }
+  else if(request.headers['accept'].includes('text/plain')){
+    readFileSendResponse('data.txt', 'text/plain', response);
+  }
+  else if(request.headers['accept'].includes('*/*')){
+    readFileSendResponse('data.txt', '*/*', response);
+  }
+  else {
     response.statusCode = 406;
     response.statusMessage = 'Content type not available';
     response.end();
