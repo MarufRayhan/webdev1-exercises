6723afe3ffedf492029942f8221b1b4291d1ef61
maruf rayhan <maruf.rayhan@tuni.fi>
2022-09-16 14:21:38 +0300

    router added

--- exercises/02_http/2.6-routing/test.js
+++ exercises/02_http/2.6-routing/test.js
@@ -1,61 +1,36 @@
-// const http = require('http');
-// const fs = require('fs');
-// const path = require('path');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-// http.createServer(function(request, response) {
+http.createServer(function(request, response) {
+    if (request.url === '/respond'){
+    response.writeHead(200, { 'Content-Type':'text/html'});
+    readFileSendResponse('index.html', 'text/html', response);
+  }
+  else if (request.url === '/dystopy'){
+    response.writeHead(200, { 'Content-Type':'text/html'});
+    readFileSendResponse('bradbury.html', 'text/html', response);
+  }
+  else if (request.url === '/classical'){
+    response.writeHead(200, { 'Content-Type':'text/html'});
+    readFileSendResponse('homer.html', 'text/html', response);
+  }
+  else {
+    response.statusCode = 404;
+    response.statusMessage = 'Requested content not found';
+    response.end(response.statusMessage);
+  }
+}).listen(3000);
 
-//   //TODO: implement sending responds to requests for XML, TXT, and */* below, handling JSON provided as an example.
-//   if (request.headers['accept'].includes('application/json')) {
-//     readFileSendResponse('data.json', 'application/json', response);
-//   } 
-//   else if(request.headers['accept'].includes('application/xml')){
-//     readFileSendResponse('data.xml', 'application/xml', response);
-//   }
-//   else if(request.headers['accept'].includes('text/plain')){
-//     readFileSendResponse('data.txt', 'text/plain', response);
-//   }
-//   else if(request.headers['accept'].includes('*/*')){
-//     readFileSendResponse('data.txt', '*/*', response);
-//   }
-//   else {
-//     response.statusCode = 406;
-//     response.statusMessage = 'Content type not available';
-//     response.end();
-//   }
-// }).listen(3000);
-
-// const readFileSendResponse = (fileName, contentType, response) => {
-//   fs.readFile(path.resolve(fileName), function(error, file) {
-//     if (error) {
-//       response.writeHead(404);
-//       response.write('An error occured: ', error);
-//     } else {
-//       response.writeHead(200, { 'Content-Type': contentType });
-//       response.write(file);
-//     }
-//     response.end();
-//   })
-// }
-
-var page = require('webpage').create(),
-    system = require('system'),
-    resources = [];
-
-page.open('http://google.com', function (status) {
-    console.log('Loaded with http status:', resources[0].status);
-    phantom.exit();
-});
-
-page.onResourceReceived = function(response) {
-    // check if the resource is done downloading 
-    if (response.stage !== "end") return;
-    // apply resource filter if needed:
-    if (response.headers.filter(function(header) {
-        if (header.name == 'Content-Type' && header.value.indexOf('text/html') == 0) {
-            response.statuscode = 404
-            return response.statuscode;
-        }
-        return false;
-    }).length > 0)
-        resources.push(response);
-};
+const readFileSendResponse = (fileName, contentType, response) => {
+  fs.readFile(path.resolve(fileName), function(error, file) {
+    if (error) {
+        response.writeHead(404);
+        response.write('Requested content not found');
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.write(file);
+    }
+    response.end();
+  })
+}
