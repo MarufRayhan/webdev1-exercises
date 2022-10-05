4107c4d758c2646f0134971b273349be775e479a
maruf rayhan <maruf.rayhan@tuni.fi>
2022-10-06 02:28:31 +0300

    first phase done

---
+++ exercises/05_javascript/5.4-events-triggered-and-emitted/custom-events.js
@@ -0,0 +1,35 @@
+const one = document.getElementById("ones").childNodes;
+let len = document.getElementById("ones").childNodes.length;
+
+const alldots = Array.from(document.querySelectorAll("#rolls div"));
+const dice = document
+  .getElementById("roll-button")
+  .addEventListener("click", (e) => {
+    rollDice();
+  });
+
+document.addEventListener("rollDice", function (e) {
+  diceValue = e.detail.value;
+  console.log(diceValue);
+
+  if (diceValue === 1) {
+    document.getElementById("ones").childNodes[3].textContent = diceValue;
+  } else if (diceValue === 2) {
+    document.getElementById("twos").childNodes[3].textContent = diceValue;
+  } else if (diceValue === 3) {
+    document.getElementById("threes").childNodes[3].textContent = diceValue;
+  } else if (diceValue === 4) {
+    document.getElementById("fours").childNodes[3].textContent = diceValue;
+  } else if (diceValue === 5) {
+    document.getElementById("fives").childNodes[3].textContent = diceValue;
+  } else if (diceValue === 6) {
+    document.getElementById("sixes").childNodes[3].textContent = diceValue;
+  } else {
+    console.log("error");
+  }
+
+  let haha = document.querySelector("#totals");
+  haha.innerHTML = `<p>Total rolls: <span>${diceValue}</span></p>`;
+  // let spanTag = document.getElementById("totals").childNodes[0]
+  // spanTag.innerHTML = e.detail.value;
+});
