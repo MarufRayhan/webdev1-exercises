7adada03e764132f0434e028bf977ff836d67d4b
maruf rayhan <maruf.rayhan@tuni.fi>
2022-10-06 02:32:09 +0300

    first phase done

--- exercises/05_javascript/5.4-events-triggered-and-emitted/custom-events.js
+++ exercises/05_javascript/5.4-events-triggered-and-emitted/custom-events.js
@@ -1,10 +1,9 @@
-const one = document.getElementById("ones").childNodes;
-let len = document.getElementById("ones").childNodes.length;
-
+count = 0;
 const alldots = Array.from(document.querySelectorAll("#rolls div"));
 const dice = document
   .getElementById("roll-button")
   .addEventListener("click", (e) => {
+    count++;
     rollDice();
   });
 
@@ -29,7 +28,7 @@ document.addEventListener("rollDice", function (e) {
   }
 
   let haha = document.querySelector("#totals");
-  haha.innerHTML = `<p>Total rolls: <span>${diceValue}</span></p>`;
+  haha.innerHTML = `<p>Total rolls: <span>${count}</span></p>`;
   // let spanTag = document.getElementById("totals").childNodes[0]
   // spanTag.innerHTML = e.detail.value;
 });
