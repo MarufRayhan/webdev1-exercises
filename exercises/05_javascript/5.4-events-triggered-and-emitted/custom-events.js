5f2be36c6908a2d2bfacd441b347d880f8fa7337
maruf rayhan <maruf.rayhan@tuni.fi>
2022-10-06 14:53:57 +0300

    first phase done

--- exercises/05_javascript/5.4-events-triggered-and-emitted/custom-events.js
+++ exercises/05_javascript/5.4-events-triggered-and-emitted/custom-events.js
@@ -1,34 +1,68 @@
-count = 0;
-const alldots = Array.from(document.querySelectorAll("#rolls div"));
+//DICE 1
+const DICE_ONE = document.getElementById("ones").childNodes[3];
+const ELEMENT_FRAGMENT_1 = document.getElementById("template1").content;
+//DICE 2
+const DICE_TWO = document.getElementById("twos").childNodes[3];
+const ELEMENT_FRAGMENT_2 = document.getElementById("template2").content;
+//DICE 3
+const DICE_THREE = document.getElementById("threes").childNodes[3];
+const ELEMENT_FRAGMENT_3 = document.getElementById("template3").content;
+//DICE 4
+const DICE_FOUR = document.getElementById("fours").childNodes[3];
+const ELEMENT_FRAGMENT_4 = document.getElementById("template4").content;
+//DICE 5
+const DICE_FIVE = document.getElementById("fives").childNodes[3];
+const ELEMENT_FRAGMENT_5 = document.getElementById("template5").content;
+//DICE 6
+const DICE_SIX = document.getElementById("sixes").childNodes[3];
+const ELEMENT_FRAGMENT_6 = document.getElementById("template6").content;
+
+COUNT = 0;
+// const alldots = Array.from(document.querySelectorAll("#rolls div"));
+
 const dice = document
   .getElementById("roll-button")
   .addEventListener("click", (e) => {
-    count++;
+    COUNT++;
     rollDice();
   });
 
+function changeRollButton(template) {
+  const changeButton = document.getElementById("roll-button");
+  changeButton.querySelector("span").replaceWith(template);
+}
+
 document.addEventListener("rollDice", function (e) {
   diceValue = e.detail.value;
-  console.log(diceValue);
 
   if (diceValue === 1) {
-    document.getElementById("ones").childNodes[3].textContent = diceValue;
+    DICE_ONE.textContent = diceValue;
+    var template = document.importNode(ELEMENT_FRAGMENT_1, true);
+    changeRollButton(template);
   } else if (diceValue === 2) {
-    document.getElementById("twos").childNodes[3].textContent = diceValue;
+    DICE_TWO.textContent = diceValue;
+    var template = document.importNode(ELEMENT_FRAGMENT_2, true);
+    changeRollButton(template);
   } else if (diceValue === 3) {
-    document.getElementById("threes").childNodes[3].textContent = diceValue;
+    DICE_THREE.textContent = diceValue;
+    var template = document.importNode(ELEMENT_FRAGMENT_3, true);
+    changeRollButton(template);
   } else if (diceValue === 4) {
-    document.getElementById("fours").childNodes[3].textContent = diceValue;
+    DICE_FOUR.textContent = diceValue;
+    var template = document.importNode(ELEMENT_FRAGMENT_4, true);
+    changeRollButton(template);
   } else if (diceValue === 5) {
-    document.getElementById("fives").childNodes[3].textContent = diceValue;
+    DICE_FIVE.textContent = diceValue;
+    var template = document.importNode(ELEMENT_FRAGMENT_5, true);
+    changeRollButton(template);
   } else if (diceValue === 6) {
-    document.getElementById("sixes").childNodes[3].textContent = diceValue;
+    DICE_SIX.textContent = diceValue;
+    var template = document.importNode(ELEMENT_FRAGMENT_6, true);
+    changeRollButton(template);
   } else {
-    console.log("error");
+    console.log("No data!");
   }
 
-  let haha = document.querySelector("#totals");
-  haha.innerHTML = `<p>Total rolls: <span>${count}</span></p>`;
-  // let spanTag = document.getElementById("totals").childNodes[0]
-  // spanTag.innerHTML = e.detail.value;
+  let totalCount = document.querySelector("#totals").querySelector("span");
+  totalCount.innerHTML = diceValue;
 });
