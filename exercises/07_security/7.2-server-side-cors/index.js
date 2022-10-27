5f5ea470c1d36a591910141ff8dd0b2340ce2e7a
maruf rayhan <maruf.rayhan@tuni.fi>
2022-10-27 20:33:43 +0300

    primary fix

--- exercises/07_security/7.2-server-side-cors/index.js
+++ exercises/07_security/7.2-server-side-cors/index.js
@@ -3,65 +3,36 @@ const port = 3000;
 
 http
   .createServer((req, res) => {
+    if (req.headers.origin == null) {
+      res.writeHead(400, { "Content-Type": "text/plain" });
+      res.write("Origin header not in the request");
+
+      res.end();
+      return;
+    }
     const headers = {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Access-Control-Allow-Methods": "HEAD, GET, POST",
       "Access-Control-Max-Age": 7200,
     };
 
-    const originName = req.headers["origin"];
-
-    if (!req.headers[originName]) {
-      res.writeHead(400);
-      res.end("Origin header not in the request");
-      return;
-    }
-
-    if (req.method === "OPTIONS") {
-      res.writeHead(200, headers);
-      res.end();
-      return;
-    }
-
-    if (["HEAD"].indexOf(req.method) > -1) {
-      res.writeHead(200, headers);
-      res.end();
-      return;
-    }
-
-    if (["GET", "POST"].indexOf(req.method) > -1) {
+    if (
+      req.method === "HEAD" ||
+      req.method === "GET" ||
+      req.method === "POST"
+    ) {
       res.writeHead(200, headers);
       res.end("I was requested using CORS!");
       return;
     }
 
-    res.writeHead(200, headers);
-    res.end(`${req.method} is not allowed for the request.`);
+    if (!req.headers["origin"]) {
+      res.writeHead(400, headers);
+      res.end("Origin header not in the request");
+      return;
+    }
 
-    // TODO: handle HEAD HTTP method,
-    // remember to add CORS headers to response
-
-    // TODO: handle HTTP methods that are not allowed,
-    // remember to add CORS headers to response
-
-    // HINT: remember to use end() method of the response when you are ready to send them. If you are using if-else statements, place
-    // "return;"
-    // as the last line of all if-else branches.
-    // So, something like:
-    // if(condition){
-    //     ....
-    //     response.end();
-    //     return;
-    // }
-    // else if(condition){
-    //     ....
-    //     response.end();
-    //     return;
-    // }
-    // else{
-    //     ....
-    //     response.end();
-    //     return;
-    // }
+    res.writeHead(405, headers);
+    res.end(`Request used a HTTP method which is not allowed.`);
   })
   .listen(port);
